3e6aaf6944c6358b227e81540531d4461fe5c0c2
henipatell <henipatel8913@gmail.com>
2020-11-04 23:57:06 -0500

    small changes into the templates

--- application/routes.py
+++ application/routes.py
@@ -1,19 +1,27 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, json, Response
 
 @app.route("/")
-@app.route("/search")
-def search():
-    return render_template("search.html")
+@app.route("/search/")
+@app.route("/search/<searchTerm>")
+def search(searchTerm=None):
+    return render_template("search.html", searchTerm=searchTerm)
+
+@app.route("/search-results", methods=["POST"])
+def search_results():
+    searchTerm = request.form.get('searchTerm')
+    print(searchTerm)
+    return render_template("search_results.html", searchTerm=searchTerm)
+
 
 @app.route("/home")
 def home():
-    return render_template("home.html", login=False)
+    return render_template("home.html", home=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html", register=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)
+    return render_template("login.html", login=True)
