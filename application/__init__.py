cc5a2967d3673c91f0adeb3c9cee88c322bfc602
henipatell <henipatel8913@gmail.com>
2020-11-11 17:59:35 -0500

    small fixes

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask, flash, request, redirect, url_for
 from config import Config
 from flask_mongoengine import MongoEngine
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 
 ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-Bootstrap(app)
+#Bootstrap(app)
 
 app.config['DEBUG'] = True
 app.config['PDF_DIR_LOC'] = './app/static/'
