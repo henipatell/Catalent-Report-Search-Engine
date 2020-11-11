185d885adbdd3b690a469e330d02bedee41fdf53
Heni Patel <henipatel8913@gmail.com>
2020-11-11 16:07:50 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "dsdfsvcbcvmbcvmcvcvdfjgndifvndnvdjnvjnxvcvnxjnvdjfdsvvnxj238rryh7ery74r"
-    
-    MONGODB_SETTINGS = {'db' : 'catalent_reports_db', 'host' : 'mongodb+srv://henipatel:henipatel@cluster0.fjtgv.mongodb.net/catalent_reports_db?retryWrites=true&w=majority'}
