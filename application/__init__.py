c6f57243d4aede6c651b38612e0b54f33c8ea51f
henipatell <henipatel8913@gmail.com>
2020-11-25 16:12:40 -0500

    UI changes

--- application/__init__.py
+++ application/__init__.py
@@ -11,11 +11,6 @@ app.config.from_object(Config)
 #Bootstrap(app)
 
 app.config['DEBUG'] = True
-#app.config['PDF_DIR'] = 'D:\Internship-Project\TempProject\sample_pdf_files\' 
-app.config['DB_PATH'] = './app/sql/'
-app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 #10 Mo size max for upload
-app.config['ALLOW_UPLOAD'] = True
-app.config['UPLOAD_FOLDER'] = './app/uploads'
 
 db = MongoEngine()
 db.init_app(app)
